64ae8f75c0f5dea3e7536f0727a99dfd0d8bc44e
Tahmied <tahmiedhossain4671@gmail.com>
2025-04-08 17:18:52 +0600

    implement user model with password encryption and token generation, update registerUser for structured response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,27 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req,res)=> {
-    return res.json(
-        {
-            message : "ok"
-        }
+    const {userName , email, password} = req.body
+
+    if([userName,password].some((e)=> !e)) {
+        throw new ApiError(400 , "all fields are required")
+    }
+
+    const user = await User.create({
+        userName : userName,
+        email : email,
+        password: password
+    })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200 , user , 'user registration successful')
     )
+
 } )
 
 export { registerUser };
---
+++ src/models/user.model.js
@@ -0,0 +1,64 @@
+import { jwt } from "jsonwebtoken";
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    userName : {
+        type: String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,
+        index:true
+    } , 
+    email : {
+        type: String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true
+    },
+    password : {
+        type: String,
+        required: [true, 'Password is required']
+    }
+})
+
+// encrypt the password before saving
+userSchema.pre('save' , async function (next) {
+    if(!this.isModified('password')) return next()
+
+    this.password = await bcrypt.hash(this.password , 10)
+    next()
+})
+
+// function to compare passwords for login
+userSchema.methods.checkPassword = async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+
+// generate access and refresh Token
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id : this._id,
+        userName : this.userName,
+        email : this.email
+    }), process.env.ACCESS_TOKEN_SECRET ,
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id : this._id,
+        email : this.email
+    }), process.env.REFRRESH_TOKEN_SECRET ,
+    {
+        expiresIn : process.env.REFRRESH_TOKEN_EXPIRY
+    }
+}
+
+
+
+
+export const User = mongoose.model('User' , userSchema)
